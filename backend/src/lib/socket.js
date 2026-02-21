ddfedab1c6990551c4fd8d8dd62590b67d102563
Sunny Singh <sunnysingh54910@gmail.com>
2026-02-21 11:44:09 +0530

    feat: add real-time typing indicators via socket.io
    - Listen for 'typingStart' event and emit 'userTyping' to receiver
    - Listen for 'typingStop' event and emit 'userStoppedTyping' to receiver
    - Enables live typing status in chat UI

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: true, // Allow all origins
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Typing indicators
+  socket.on("typingStart", ({ receiverId }) => {
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("userTyping", { senderId: userId });
+    }
+  });
+
+  socket.on("typingStop", ({ receiverId }) => {
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("userStoppedTyping", { senderId: userId });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { io, app, server };
