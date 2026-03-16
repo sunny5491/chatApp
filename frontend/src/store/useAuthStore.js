e317485cc7fe1f3ec7601c8cb7e6f1ebb2bd711f
Sunny Singh <sunnysingh54910@gmail.com>
2026-03-17 01:19:27 +0530

    fix:bugs

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -31,6 +31,7 @@ export const useAuthStore = create((set, get) => ({
   signup: async (data) => {
     set({ isSigningUp: true });
     try {
+
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
